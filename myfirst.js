d24ecd7bed3180e1fa3abe31a4ea9e3a3da96178
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-24 16:40:18 +0200

    Created a module for displaying time and tested it

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Tere maailm!');
+    res.end('Tere maailm! Kell on: ' +dt.myDateTime());
 }).listen(8080);
