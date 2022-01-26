82e65a4770bfff91174853eda327c8df4f68d9bd
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-26 13:58:53 +0200

    Added req.url element

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,9 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
+//var dt = require('./myfirstmodule');
 
 //create server object
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'}); //write a response to the client
-    res.end('Tere maailm! Kell on: ' +dt.myDateTime()); //end the response
+    res.write(req.url);
+    res.end();
 }).listen(8080); //the server object listens on port 8080
