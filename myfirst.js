5399b4d9e45d3d1e3ff07e2ca65e914d729dcecf
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-26 13:50:25 +0200

    Added comments to explain what every piece of code does

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
 
+//create server object
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Tere maailm! Kell on: ' +dt.myDateTime());
-}).listen(8080);
+    res.writeHead(200, {'Content-Type': 'text/html'}); //write a response to the client
+    res.end('Tere maailm! Kell on: ' +dt.myDateTime()); //end the response
+}).listen(8080); //the server object listens on port 8080
