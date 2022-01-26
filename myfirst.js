d0f100958401ddb35841bbad3babe828a869490f
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-26 14:06:12 +0200

    Split the query string

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,11 @@
-var http = require('http');
+var http = require("http");
+var url = require("url");
 //var dt = require('./myfirstmodule');
 
 //create server object
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'}); //write a response to the client
-    res.write(req.url);
-    res.end();
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
 }).listen(8080); //the server object listens on port 8080
