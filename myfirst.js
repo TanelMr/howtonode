73d669a8395f6e0505f311de0f8466790efc8cc8
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-26 18:23:48 +0200

    Installing and using uppercase module

--- myfirst.js
+++ myfirst.js
@@ -1,22 +1,14 @@
 var http = require("http");
-var url = require("url");
-var fs = require("fs");
+//var url = require("url");
+//var fs = require("fs");
+var uc = require("upper-case");
 
 //var dt = require('./myfirstmodule');
 
-//create server object
 http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(uc.upperCase("Hello World!"));
+    res.end();
 }).listen(8080);
 
 /*
