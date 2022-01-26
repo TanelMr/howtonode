14167e1e6d6a1a4d41fd64e31581e6c2bfaeda15
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-26 14:11:34 +0200

    Let node read from html file

--- myfirst.js
+++ myfirst.js
@@ -1,11 +1,13 @@
 var http = require("http");
-var url = require("url");
+//var url = require("url");
+var fs = require("fs");
 //var dt = require('./myfirstmodule');
 
 //create server object
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write a response to the client
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
 }).listen(8080); //the server object listens on port 8080
