1415cf36f173d5b9b5486410a2aa4ef530641ee3
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-26 14:15:22 +0200

    Created a new file using fs.appendfile

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,10 @@
-var http = require("http");
+//var http = require("http");
 //var url = require("url");
 var fs = require("fs");
 //var dt = require('./myfirstmodule');
 
 //create server object
+/*
 http.createServer(function (req, res) {
     fs.readFile('index.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
@@ -11,3 +12,10 @@ http.createServer(function (req, res) {
         return res.end();
     });
 }).listen(8080); //the server object listens on port 8080
+ */
+
+//create a file named newfile.txt:
+fs.appendFile('newfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Salvestatud!');
+});
