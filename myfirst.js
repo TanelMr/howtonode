31cc3d7d432802890039e5b32a1d299e71398b71
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-26 18:17:47 +0200

    Example of node acting as a server

--- myfirst.js
+++ myfirst.js
@@ -1,21 +1,27 @@
-//var http = require("http");
-//var url = require("url");
+var http = require("http");
+var url = require("url");
 var fs = require("fs");
+
 //var dt = require('./myfirstmodule');
 
 //create server object
-/*
 http.createServer(function (req, res) {
-    fs.readFile('index.html', function(err, data) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
     });
-}).listen(8080); //the server object listens on port 8080
- */
+}).listen(8080);
 
-//delete file:
+/*
 fs.rename('newfile1.txt', "uusfailinimi.txt", function (err) {
     if (err) throw err;
     console.log('File renamed!');
 });
+*/
